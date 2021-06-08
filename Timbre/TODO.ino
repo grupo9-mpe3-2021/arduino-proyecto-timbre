663c6bdfe6dc10dcba76e60d1e448b86d3b5ce48
KrappRamiro <83929485+KrappRamiro@users.noreply.github.com>
2021-06-08 15:14:40 -0300

    V1.0
    Subi los archivos que tenemos hasta la fecha del codigo exclusivamente

---
+++ Timbre/TODO.ino
@@ -0,0 +1,24 @@
+/*
+ *  Codigos usados de ejemplo y referencias:
+ *      > https://www.luisllamas.es/reloj-y-calendario-en-arduino-con-los-rtc-ds1307-y-ds3231/
+ *      > https://adafruit.github.io/RTClib/html/class_r_t_c___d_s3231.html#a097dc15a79ec077073969432b1abcb88
+ *      > https://arduwiki.perut.org/wiki/sprintf() //El arduino no toma () como parte de la url, eso es un bug
+ *      >https://github.com/garrysblog/DS3231-Alarm-With-Adafruit-RTClib-Library/blob/master/DS3231-RTClib-Adafruit-Alarm-Poll-alarmFired/DS3231-RTClib-Adafruit-Alarm-Poll-alarmFired.ino
+ * 
+ * 
+ *  TODO: 
+ *  Agregar Menus:
+ *      > Agregar menu para agregar alarmas
+ *      > Agregar menu para ver las alarmas y poder eliminarlas
+ *      > Agregar la opcion de hacer sonar la alarma de forma manual con un boton
+ *  
+ *      
+ *
+ *  BUGS QUE ARREGLAR:
+ *      >
+ *      >     
+ *      
+ *    
+ *  Agregar prendido y apagado automatico (que sea configurable)
+ *  
+ */
